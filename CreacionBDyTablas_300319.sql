f75323a3b4abbb3fe8d1b695470c8ce5a6d719c9
Sebastian <sebastianvalera99@outlook.com>
2019-04-20 22:15:44 -0700

    Se actualizo el archivo sql con los querys de creacion de tablas

--- CreacionBDyTablas_300319.sql
+++ CreacionBDyTablas_300319.sql
@@ -2,8 +2,10 @@ CREATE DATABASE AESEMBD
 USE AESEMBD
 CREATE TABLE USUARIOS (Id_Usuarios int primary key identity, Nickname varchar(15),
 Nombre varchar(20),Ape_Pat varchar(12), Ape_Mat varchar(12), Password varchar(20))
-CREATE TABLE EMPRESAS (Id_Empresas int primary key identity, Nombre varchar(50), Logo image,
-Lugar varchar(30),TipoPago int, Sindicato varchar(200), Concepto varchar(200), Comentarios varchar(255))
-CREATE TABLE Sucursales(Id_Sucursales int primary key,
+CREATE TABLE EMPRESAS (Id_Empresas int primary key identity, Logo image,
+Lugar varchar(30),TipoPago int, Sindicato varchar(200), Concepto varchar(200), Ruta varchar(100))
+CREATE TABLE SUCURSALES(Id_Sucursales int primary key,
 Id_Empresas int foreign key references Empresas(Id_Empresas) on update cascade on delete cascade,
 Nombre varchar(30), Direccion varchar(50))
+CREATE TABLE EMPLEADOS (Id_Empleados int primary key identity, Nombre varchar(100), 
+NoCuenta varchar(50), Importe float, Periodo varchar(50))
