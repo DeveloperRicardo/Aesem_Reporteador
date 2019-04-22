7c0f178fa7190dc61fd87018d9bed9523c84923c
Sebastian <sebastianvalera99@outlook.com>
2019-04-21 19:23:14 -0700

    Se resolvieron conflictos de merge en GIT

--- AESEM_Reporteador/AESEM_Reporteador/WIN_Exportar_P.cs
+++ AESEM_Reporteador/AESEM_Reporteador/WIN_Exportar_P.cs
@@ -7,8 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using iTextSharp.text;
-using iTextSharp.text.pdf;
+//using iTextSharp.text;
+//using iTextSharp.text.pdf;
 using System.IO;
 
 namespace AESEM_Reporteador
@@ -21,47 +21,47 @@ namespace AESEM_Reporteador
         }
         public void Pdf_Exportar(object sender, EventArgs e)
         {
-            string filename = "test";
-            DataGridView dgw = dataGridView1;
-            BaseFont bf = BaseFont.CreateFont(BaseFont.TIMES_ROMAN, BaseFont.CP1250, BaseFont.EMBEDDED);
-            PdfPTable pdfPTable = new PdfPTable(dgw.Columns.Count);
-            pdfPTable.DefaultCell.Padding = 3;
-            pdfPTable.WidthPercentage = 100;
-            pdfPTable.HorizontalAlignment = Element.ALIGN_LEFT;
-            pdfPTable.DefaultCell.BorderWidth = 1;
+            //string filename = "test";
+            //DataGridView dgw = dataGridView1;
+            //BaseFont bf = BaseFont.CreateFont(BaseFont.TIMES_ROMAN, BaseFont.CP1250, BaseFont.EMBEDDED);
+            //PdfPTable pdfPTable = new PdfPTable(dgw.Columns.Count);
+            //pdfPTable.DefaultCell.Padding = 3;
+            //pdfPTable.WidthPercentage = 100;
+            //pdfPTable.HorizontalAlignment = Element.ALIGN_LEFT;
+            //pdfPTable.DefaultCell.BorderWidth = 1;
 
-            iTextSharp.text.Font text = new iTextSharp.text.Font(bf, 10, iTextSharp.text.Font.NORMAL);
+            //iTextSharp.text.Font text = new iTextSharp.text.Font(bf, 10, iTextSharp.text.Font.NORMAL);
 
-            foreach (DataGridViewColumn column in dgw.Columns)
-            {
-                PdfPCell cell = new PdfPCell(new Phrase(column.HeaderText, text));
-                cell.BackgroundColor = new iTextSharp.text.BaseColor(240, 240, 240);
-                pdfPTable.AddCell(cell);
-            }
+            //foreach (DataGridViewColumn column in dgw.Columns)
+            //{
+            //    PdfPCell cell = new PdfPCell(new Phrase(column.HeaderText, text));
+            //    cell.BackgroundColor = new iTextSharp.text.BaseColor(240, 240, 240);
+            //    pdfPTable.AddCell(cell);
+            //}
 
-            foreach (DataGridViewRow row in dgw.Rows)
-            {
-                foreach (DataGridViewCell cell in row.Cells)
-                {
-                    pdfPTable.AddCell(new Phrase(cell.Value.ToString(), text));
-                }
-            }
+            //foreach (DataGridViewRow row in dgw.Rows)
+            //{
+            //    foreach (DataGridViewCell cell in row.Cells)
+            //    {
+            //        pdfPTable.AddCell(new Phrase(cell.Value.ToString(), text));
+            //    }
+            //}
 
-            var savefiledialog = new SaveFileDialog();
-            savefiledialog.FileName = filename;
-            savefiledialog.DefaultExt = ".pdf";
-            if (savefiledialog.ShowDialog() == DialogResult.OK)
-            {
-                using (FileStream stream = new FileStream(savefiledialog.FileName, FileMode.Create))
-                {
-                    Document pdfoc = new Document(PageSize.A4, 10f, 10f, 10f, 0f);
-                    PdfWriter.GetInstance(pdfoc, stream);
-                    pdfoc.Open();
-                    pdfoc.Add(pdfPTable);
-                    pdfoc.Close();
-                    stream.Close();
-                }
-            }
+            //var savefiledialog = new SaveFileDialog();
+            //savefiledialog.FileName = filename;
+            //savefiledialog.DefaultExt = ".pdf";
+            //if (savefiledialog.ShowDialog() == DialogResult.OK)
+            //{
+            //    using (FileStream stream = new FileStream(savefiledialog.FileName, FileMode.Create))
+            //    {
+            //        Document pdfoc = new Document(PageSize.A4, 10f, 10f, 10f, 0f);
+            //        PdfWriter.GetInstance(pdfoc, stream);
+            //        pdfoc.Open();
+            //        pdfoc.Add(pdfPTable);
+            //        pdfoc.Close();
+            //        stream.Close();
+            //    }
+            //}
         }
 
         private void WIN_Exportar_P_Load(object sender, EventArgs e)
